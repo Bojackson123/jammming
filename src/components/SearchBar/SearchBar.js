7a35cc16273e8432e91488640cd376588e021a0d
Rashid <rashidiscool21@gmail.com>
2024-05-22 06:54:14 +0100

    reverted refresh token change

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,39 +1,26 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import './SearchBar.css';
 import Tracklist from '../Tracklist/Tracklist';
 import Playlist from '../Playlist/Playlist';
 import spotifyService from '../../spotifyService';
 import SpotifyAuth from '../SpotifyAuth/SpotifyAuth';
 
+
 function SearchBar() {
     const [isAuthenticated, setIsAuthenticated] = useState(false);
     const [userName, setUserName] = useState('');
 
     useEffect(() => {
-        // Function to check URL for code and handle authentication
-        const handleAuthentication = async () => {
-            const urlParams = new URLSearchParams(window.location.search);
-            const code = urlParams.get('code');
-            if (code) {
-                await spotifyService.fetchAccessToken(code);
-            }
-            const token = spotifyService.getAccessToken();
-            console.log('Access token:', token);
-            console.log('Refresh token:', spotifyService.refreshToken);
-            if (token) {
-                setIsAuthenticated(true);
-                spotifyService.getUserProfile().then(profile => {
-                    if (profile) {
-                        setUserName(profile.display_name || 'User');
-                    }
-                }).catch(err => console.error('Failed to fetch user profile:', err));
-            }
-        };
-
-        handleAuthentication();
+        const token = spotifyService.getAccessToken();
+        if (token) {
+            setIsAuthenticated(true);
+            spotifyService.getUserProfile().then(profile => {
+                if (profile) {
+                    setUserName(profile.display_name);
+                }
+            });
+        }
     }, []);
-
-
     return (
         <>
             <div className='spotify-auth'>
@@ -56,13 +43,15 @@ function SearchBar() {
                 <div className='playlist'>
                     <Playlist />
                     <div className='button'>
-                        <button className='playlist-save'>SAVE TO SPOTIFY</button>
-                        <button className='playlist-clear'>CLEAR PLAYLIST</button>
+                    <button className='playlist-save'>SAVE TO SPOTIFY</button>
+                    <button className='playlist-clear'>CLEAR PLAYLIST</button>
                     </div>
                 </div>
             </div>
         </>
+       
+        
     );
-}
+};
 
 export default SearchBar;
--- src/spotifyService.js
+++ src/spotifyService.js
@@ -2,177 +2,131 @@ import axios from 'axios';
 
 class SpotifyService {
     constructor() {
-        this.clientId = '0147af87650b4d0a9b79ce5996311e55'; // Replace with your Spotify client ID
-        this.redirectUri = 'https://jammming-sounds.vercel.app/'; // Replace with your redirect URI
-        this.scopes = 'playlist-modify-public';
-        this.accessToken = null;
-        this.refreshToken = null;
-        this.codeVerifier = null;
+      this.accessToken = null;
+      this.clientId = '0147af87650b4d0a9b79ce5996311e55'; // Replace with your Spotify client ID
+      this.redirectUri = 'https://jammming-sounds.vercel.app/'; // Replace with your redirect URI
+      this.scopes = 'playlist-modify-public';
     }
-
-    // Generate a secure random string for the PKCE challenge
-    generateRandomString(length) {
-        let text = '';
-        const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~';
-        for (let i = 0; i < length; i++) {
-            text += possible.charAt(Math.floor(Math.random() * possible.length));
-        }
-        return text;
+  
+    setAccessToken(token) {
+      this.accessToken = token;
+      localStorage.setItem('spotify_access_token', token);
     }
-
-    // Generate code challenge from verifier
-    async generateCodeChallenge(codeVerifier) {
-        const encoder = new TextEncoder();
-        const data = encoder.encode(codeVerifier);
-        const digest = await crypto.subtle.digest('SHA-256', data);
-        const base64Digest = btoa(String.fromCharCode(...new Uint8Array(digest)));
-        return base64Digest.replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '');
-    }
-
-    // Store tokens in local storage to maintain session state
-    setTokens(accessToken, refreshToken = null) {
-        this.accessToken = accessToken;
-        this.refreshToken = refreshToken;
-        localStorage.setItem('spotify_access_token', accessToken);
-        if (refreshToken) {
-            localStorage.setItem('spotify_refresh_token', refreshToken);
-        }
-    }
-
-    // Start authorization process
-    async authorize() {
-        this.codeVerifier = this.generateRandomString(128);
-        const codeChallenge = await this.generateCodeChallenge(this.codeVerifier);
-        const authUrl = `https://accounts.spotify.com/authorize?response_type=code&client_id=${this.clientId}&scope=${encodeURIComponent(this.scopes)}&redirect_uri=${encodeURIComponent(this.redirectUri)}&code_challenge_method=S256&code_challenge=${codeChallenge}`;
-        window.location.href = authUrl;
-    }
-
-    // Retrieves the access token from internal state or local storage
+  
     getAccessToken() {
+      // Check if the access token is already present in the instance or in the local storage
       if (this.accessToken) {
-          return this.accessToken;
+        return this.accessToken;
       }
-
-      const storedAccessToken = localStorage.getItem('spotify_access_token');
-      if (storedAccessToken) {
-          this.accessToken = storedAccessToken;
-          return storedAccessToken;
+  
+      const storedToken = localStorage.getItem('spotify_access_token');
+      if (storedToken) {
+        this.accessToken = storedToken;
+        return storedToken;
       }
-
+  
+      // Check the URL for the access token
+      const hash = window.location.hash;
+      if (hash) {
+        const params = new URLSearchParams(hash.substring(1));
+        const accessToken = params.get('access_token');
+        if (accessToken) {
+          this.setAccessToken(accessToken);
+          window.location.hash = '';
+          return accessToken;
+        }
+      }
+  
       return null;
     }
+  
+    authorize() {
+      const authUrl = `https://accounts.spotify.com/authorize?response_type=token&client_id=${this.clientId}&scope=${encodeURIComponent(this.scopes)}&redirect_uri=${encodeURIComponent(this.redirectUri)}`;
+      window.location.href = authUrl;
+    }
+  
+    async search(query) {
+      const url = `https://api.spotify.com/v1/search?q=${query}&type=track,album,artist`;
+      const headers = {
+        'Authorization': `Bearer ${this.getAccessToken()}`
+      };
+  
+      try {
+        const response = await axios.get(url, { headers });
+        return response.data;
+      } catch (error) {
+        console.error('Error searching:', error);
+      }
+    }
+  
+    async getAlbumTracks(albumId) {
+      const url = `https://api.spotify.com/v1/albums/${albumId}/tracks`;
+      const headers = {
+        'Authorization': `Bearer ${this.getAccessToken()}`
+      };
+  
+      try {
+        const response = await axios.get(url, { headers });
+        return response.data.items;
+      } catch (error) {
+        console.error('Error fetching album tracks:', error);
+      }
+    }
+  
+    async createPlaylist(userId, name) {
+      const url = `https://api.spotify.com/v1/users/${userId}/playlists`;
+      const headers = {
+        'Authorization': `Bearer ${this.getAccessToken()}`,
+        'Content-Type': 'application/json'
+      };
+      const data = {
+        name: name,
+        public: true
+      };
+  
+      try {
+        const response = await axios.post(url, data, { headers });
+        return response.data;
+      } catch (error) {
+        console.error('Error creating playlist:', error);
+      }
+    }
+  
+    async addTracksToPlaylist(playlistId, trackUris) {
+      const url = `https://api.spotify.com/v1/playlists/${playlistId}/tracks`;
+      const headers = {
+        'Authorization': `Bearer ${this.getAccessToken()}`,
+        'Content-Type': 'application/json'
+      };
+      const data = {
+        uris: trackUris
+      };
+  
+      try {
+        await axios.post(url, data, { headers });
+        console.log('Tracks added to playlist');
+      } catch (error) {
+        console.error('Error adding tracks to playlist:', error);
+      }
+    }
 
-    // Fetch access token using authorization code
-    async fetchAccessToken(code) {
-        console.log('Attempting to fetch access token with code:', code);
-        const url = 'https://accounts.spotify.com/api/token';
-        const data = {
-            client_id: this.clientId,
-            grant_type: 'authorization_code',
-            code: code,
-            redirect_uri: this.redirectUri,
-            code_verifier: this.codeVerifier,
-        };
+    async getUserProfile() {
+        const url = 'https://api.spotify.com/v1/me';
         const headers = {
-            'Content-Type': 'application/x-www-form-urlencoded'
+            'Authorization': `Bearer ${this.getAccessToken()}`
         };
 
         try {
-            const response = await axios.post(url, new URLSearchParams(data), { headers });
-            console.log('Token response:', response.data);
-            this.setTokens(response.data.access_token, response.data.refresh_token);
-            window.history.pushState({}, null, "/"); // Clear the code from the URL
+            const response = await axios.get(url, { headers });
+            return response.data;
         } catch (error) {
-            console.error('Error fetching access token:', error);
+            console.error('Error fetching user profile:', error);
         }
     }
-
-    async refreshToken() {
-      const url = 'https://accounts.spotify.com/api/token';
-      const data = {
-          grant_type: 'refresh_token',
-          refresh_token: this.refreshToken,
-          client_id: this.clientId
-      };
-      const headers = {
-          'Content-Type': 'application/x-www-form-urlencoded'
-      };
-  
-      try {
-          const response = await axios.post(url, new URLSearchParams(data), { headers });
-          console.log('Refresh token response:', response.data);  // Debug log
-          this.setTokens(response.data.access_token);  // Assume refresh token is not returned
-      } catch (error) {
-          console.error('Error refreshing access token:', error);
-          throw new Error('Failed to refresh token'); // Handle this exception in your calling function
-      }
-  }
-  
-  async makeAuthorizedRequest(url, method = 'GET', data = null) {
-      if (!this.accessToken && localStorage.getItem('spotify_access_token')) {
-          this.accessToken = localStorage.getItem('spotify_access_token');
-      }
-  
-      console.log('Using access token:', this.accessToken);  // Debug log
-  
-      const headers = {
-          'Authorization': `Bearer ${this.accessToken}`,
-          'Content-Type': 'application/json'
-      };
-  
-      try {
-          const response = await axios({ url, method, headers, data });
-          return response.data;
-      } catch (error) {
-          console.log('Failed request with token:', this.accessToken);  // Debug log
-          if (error.response && error.response.status === 401) {
-              await this.refreshToken();
-              console.log('New access token after refresh:', this.accessToken);  // Debug log
-              return this.makeAuthorizedRequest(url, method, data);  // Retry the request with new token
-          } else {
-              console.error('Error making authorized request:', error);
-              throw error;  // Rethrow the error if it's not a 401
-          }
-      }
+    
   }
 
-    // Retrieve user profile information from Spotify
-    async getUserProfile() {
-      const url = 'https://api.spotify.com/v1/me';
-      return this.makeAuthorizedRequest(url);
-  }
-
-    // Example method: search tracks, albums, artists
-    async search(query) {
-        const url = `https://api.spotify.com/v1/search?q=${encodeURIComponent(query)}&type=track,album,artist`;
-        return this.makeAuthorizedRequest(url);
-    }
-
-    // Example method: get album tracks
-    async getAlbumTracks(albumId) {
-        const url = `https://api.spotify.com/v1/albums/${albumId}/tracks`;
-        return this.makeAuthorizedRequest(url);
-    }
-
-    // Example method: create a playlist
-    async createPlaylist(userId, name) {
-        const url = `https://api.spotify.com/v1/users/${userId}/playlists`;
-        const data = {
-            name: name,
-            public: true
-        };
-        return this.makeAuthorizedRequest(url, 'POST', data);
-    }
-
-    // Example method: add tracks to a playlist
-    async addTracksToPlaylist(playlistId, trackUris) {
-        const url = `https://api.spotify.com/v1/playlists/${playlistId}/tracks`;
-        const data = {
-            uris: trackUris
-        };
-        await this.makeAuthorizedRequest(url, 'POST', data);
-    }
-}
-
-const spotifyService = new SpotifyService();
-export default spotifyService;
+    
+  
+  const spotifyService = new SpotifyService();
+  export default spotifyService;
